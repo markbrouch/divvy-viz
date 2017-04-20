a84bfb9b9d82961bd4c95c030aaff210a6d25101
Mark Brouch <mbrouch@walmartlabs.com>
2017-04-20 16:27:25 -0500

    💄 Format with Prettier

--- server/index.js
+++ server/index.js
@@ -1,42 +1,47 @@
-const Koa = require('koa');
-const route = require('koa-route');
-const serve = require('koa-static');
-const path = require('path');
-const fs = require('fs');
-const csv = require('csv');
-const Stringify = require('streaming-json-stringify');
-const { PassThrough } = require('stream');
-const JSONStream = require('JSONStream');
+const Koa = require('koa')
+const route = require('koa-route')
+const serve = require('koa-static')
+const path = require('path')
+const fs = require('fs')
+const csv = require('csv')
+const Stringify = require('streaming-json-stringify')
+const { PassThrough } = require('stream')
+const JSONStream = require('JSONStream')
 
-const app = new Koa();
+const app = new Koa()
 
-app.use(async function (ctx, next) {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
+app.use(async function(ctx, next) {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  ctx.set('X-Response-Time', `${ms}ms`)
+})
 
-app.use(async function (ctx, next) {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-});
+app.use(async function(ctx, next) {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`)
+})
 
-app.use(serve(path.resolve(`${__dirname}/../dist`)));
+app.use(serve(path.resolve(`${__dirname}/../dist`)))
 
-app.use(route.get(`/api/trips/:stationId`, (ctx, stationId) => {
-  stationId = parseInt(stationId);
+app.use(
+  route.get(`/api/trips/:stationId`, (ctx, stationId) => {
+    stationId = parseInt(stationId)
 
-  const stream = JSONStream.stringify();
+    const stream = JSONStream.stringify()
 
-  ctx.body = stream;
-  ctx.type = 'application/json';
+    ctx.body = stream
+    ctx.type = 'application/json'
 
-  fs.createReadStream(path.resolve(`${__dirname}/grouped-trips-data-2016-q3.json`))
-    .pipe(JSONStream.parse(`${stationId}`))
-    .pipe(stream);
-}))
+    fs
+      .createReadStream(
+        path.resolve(`${__dirname}/grouped-trips-data-2016-q3.json`)
+      )
+      .pipe(JSONStream.parse(`${stationId}`))
+      .pipe(stream)
+  })
+)
 
-app.listen(process.env.PORT || 8001);
+app.listen(process.env.PORT || 8001)
--- src/app.js
+++ src/app.js
@@ -1,26 +1,25 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import MapGL from 'react-map-gl';
-import DeckGL, { IconLayer, ArcLayer } from 'deck.gl';
-import chroma from 'chroma-js';
-import values from 'lodash/values';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import MapGL from 'react-map-gl'
+import DeckGL, { IconLayer, ArcLayer } from 'deck.gl'
+import chroma from 'chroma-js'
+import values from 'lodash/values'
 
-import StationData from './station-data-2016-q3.csv';
+import StationData from './station-data-2016-q3.csv'
 
-import index from './index.html';
-import style from './app.css';
-import DivvyIcon from './images/divvy-icon.png';
+import index from './index.html'
+import style from './app.css'
+import DivvyIcon from './images/divvy-icon.png'
 
-const MAPBOX_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
+const MAPBOX_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
 
 if (!MAPBOX_TOKEN) {
-  throw new Error('Please specify a valid mapbox token');
+  throw new Error('Please specify a valid mapbox token')
 }
 
 class Root extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       width: 500,
@@ -32,30 +31,30 @@ class Root extends Component {
         bearing: 0,
         pitch: 60
       }
-    };
+    }
   }
 
   updateDimensions() {
     this.setState({
       width: window.innerWidth,
       height: window.innerHeight
-    });
+    })
   }
 
   componentWillMount() {
-    this.updateDimensions();
+    this.updateDimensions()
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this.updateDimensions.bind(this));
+    window.addEventListener('resize', this.updateDimensions.bind(this))
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions.bind(this));
+    window.removeEventListener('resize', this.updateDimensions.bind(this))
   }
 
   renderArcLayer(station) {
-    const id = parseInt(station.id);
+    const id = parseInt(station.id)
 
     this.setState({
       tripsData: [],
@@ -63,49 +62,57 @@ class Root extends Component {
         ...station,
         trips: 'loading'
       }
-    });
+    })
 
-    fetch(`/api/trips/${id}`)
-      .then(response => response.json())
-      .then(data => {
-        let trips = 0;
-        for (let toId in data[0]) {
-          trips += data[0][toId];
-          this.setState({
-            tripsData: this.state.tripsData.concat([{
+    fetch(`/api/trips/${id}`).then(response => response.json()).then(data => {
+      let trips = 0
+      for (let toId in data[0]) {
+        trips += data[0][toId]
+        this.setState({
+          tripsData: this.state.tripsData.concat([
+            {
               from: id,
               to: parseInt(toId),
               trips: data[0][toId]
-            }])
-          })
-        }
+            }
+          ])
+        })
+      }
 
-        this.setState({ selected: {...this.state.selected, trips }});
-      });
+      this.setState({ selected: { ...this.state.selected, trips } })
+    })
   }
 
   render() {
-    const {viewport, width, height, hovered = {}, selected = {}, tripsData = []} = this.state;
+    const {
+      viewport,
+      width,
+      height,
+      hovered = {},
+      selected = {},
+      tripsData = []
+    } = this.state
 
-    const stationData = values(StationData);
-    const getStationData = stationId => stationData.find(station => parseInt(station.id) === parseInt(stationId));
+    const stationData = values(StationData)
+    const getStationData = stationId =>
+      stationData.find(station => parseInt(station.id) === parseInt(stationId))
 
-    const getColor = chroma.scale(['112F54', '71870E']).domain([1,10]);
+    const getColor = chroma.scale(['112F54', '71870E']).domain([1, 10])
 
     const stationsLayer = new IconLayer({
       id: 'stations-layer',
       data: stationData,
       iconAtlas: DivvyIcon,
       iconMapping: {
-        marker: {x: 0, y: 0, width: 1024, height: 1024}
+        marker: { x: 0, y: 0, width: 1024, height: 1024 }
       },
       getPosition: d => [d.longitude, d.latitude],
       getIcon: d => 'marker',
       getSize: d => 48,
       pickable: true,
-      onHover: info => this.setState({hovered: stationData[info.index]}),
+      onHover: info => this.setState({ hovered: stationData[info.index] }),
       onClick: info => this.renderArcLayer(stationData[info.index])
-    });
+    })
 
     const tripsLayer = new ArcLayer({
       id: 'trips-layer',
@@ -113,24 +120,24 @@ class Root extends Component {
       fp64: true,
       strokeWidth: 3,
       getSourcePosition: d => {
-        const fromStation = getStationData(d.from);
+        const fromStation = getStationData(d.from)
 
-        return [fromStation.longitude, fromStation.latitude];
+        return [fromStation.longitude, fromStation.latitude]
       },
       getSourceColor: d => getColor(Math.sqrt(d.trips)).rgb(),
       getTargetColor: d => getColor(Math.sqrt(d.trips)).rgb(),
       getTargetPosition: d => {
-        const toStation = getStationData(d.to);
+        const toStation = getStationData(d.to)
 
-        return [toStation.longitude, toStation.latitude];
+        return [toStation.longitude, toStation.latitude]
       }
-    });
+    })
 
     return (
       <MapGL
         {...viewport}
         mapStyle="mapbox://styles/mapbox/light-v9"
-        onChangeViewport={v => this.setState({viewport: v})}
+        onChangeViewport={v => this.setState({ viewport: v })}
         mapboxApiAccessToken={MAPBOX_TOKEN}
         perspectiveEnabled
         width={width}
@@ -140,25 +147,33 @@ class Root extends Component {
           {...viewport}
           width={width}
           height={height}
-          layers={[ stationsLayer, tripsLayer ]}
+          layers={[stationsLayer, tripsLayer]}
         />
 
         <div className="InfoPane panel panel-default">
           <dl className="panel-body dl-horizontal">
             <dt>Name</dt><dd>{hovered.name || selected.name}</dd>
-            <dt>Capacity</dt><dd>{hovered.dpcapacity || selected.dpcapacity}</dd>
+            <dt>Capacity</dt>
+            <dd>{hovered.dpcapacity || selected.dpcapacity}</dd>
             <dt>Added</dt><dd>{hovered.online_date || selected.online_date}</dd>
-            {((!this.state.hovered && !!this.state.selected) || (this.state.hovered && this.state.selected && this.state.hovered.id === this.state.selected.id)) && <dt>Trips</dt>}
-            {((!this.state.hovered && !!this.state.selected) || (this.state.hovered && this.state.selected && this.state.hovered.id === this.state.selected.id)) && <dd>{selected.trips}</dd>}
+            {((!this.state.hovered && !!this.state.selected) ||
+              (this.state.hovered &&
+                this.state.selected &&
+                this.state.hovered.id === this.state.selected.id)) &&
+              <dt>Trips</dt>}
+            {((!this.state.hovered && !!this.state.selected) ||
+              (this.state.hovered &&
+                this.state.selected &&
+                this.state.hovered.id === this.state.selected.id)) &&
+              <dd>{selected.trips}</dd>}
           </dl>
         </div>
       </MapGL>
-    );
+    )
   }
-
 }
 
-const root = document.createElement('div');
-document.body.appendChild(root);
+const root = document.createElement('div')
+document.body.appendChild(root)
 
-render(<Root />, root);
+render(<Root />, root)
